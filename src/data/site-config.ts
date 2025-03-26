1420418202eee1145c55c9b8c09f6f99dcb03dee
Davidev22 <davidhv.dev@gmail.com>
2025-03-26 13:34:02 -0400

    Update site info

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -39,7 +39,7 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     title: 'David Henriquez',
-    subtitle: 'Here it's my personal blog',
+    subtitle: "Here it's my personal blog",
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
